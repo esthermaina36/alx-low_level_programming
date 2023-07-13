9db62a6279591581644577470166db654ef5e90f
esthermaina36 <waceramainaesther@gmail.com>
2023-07-13 14:41:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - standard error
+ * return:0;
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\", stdout);
+return(1);
+}
