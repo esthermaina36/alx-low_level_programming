b219a2bb3e4f1887f4320f032cf3d647f167b471
esthermaina36 <waceramainaesther@gmail.com>
2023-07-13 14:09:05 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - vrarious computer types.
+ * Return:0
+ */
+int main(void)
+{
+char x;
+int y;
+long int z;
+long long int w;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizof(x));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(y));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(z));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(w));
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return(0);
+}
