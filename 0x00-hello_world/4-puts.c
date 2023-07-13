bb392a3a5db4c52c4512d32c5ff33b8947d16e62
esthermaina36 <waceramainaesther@gmail.com>
2023-07-13 12:57:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,8 @@
  * main - puts function
  * return:Always 0 (success)
  */
-int main (void)
-{puts("\"Programming is like building a multilingual puzzle");
-return(0);}
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
