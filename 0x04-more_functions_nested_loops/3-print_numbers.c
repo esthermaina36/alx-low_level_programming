991365d8d8dfa24e907ea27c4ad9cce9b90ceedc
esthermaina36 <waceramainaesther@gmail.com>
2023-07-20 15:57:49 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Prints the numbers 0 to 9
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	int _putchar(char d);
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
+
