e74b49451c051eedaf15e25f6e84a58e33af486e
esthermaina36 <waceramainaesther@gmail.com>
2023-07-24 12:56:02 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - prints one character
+ * starting with the first one
+ * @str:input
+ * Return:print
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\n')
+	{
+		y++;
+		longi++;
+}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (0 % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
+
