f4e380fdab8d8e484fbd26dd5684cde3caff985c
esthermaina36 <waceramainaesther@gmail.com>
2023-08-02 07:47:24 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _puts_recursion - recursive function to print to the screen
+* @s: points to the string
+* Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
